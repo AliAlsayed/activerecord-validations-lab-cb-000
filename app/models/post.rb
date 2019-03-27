bbfe79e4e8f1a5fc1762a5f2f990ed188e0af837
Ali Alsayed <a_alsayed87@hotmail.com>
2019-03-27 06:38:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbaity
 
   def clickbaity?
     title.include?("Won't Believe") || title.include?("Top [number]") || title.include?("Guess")
